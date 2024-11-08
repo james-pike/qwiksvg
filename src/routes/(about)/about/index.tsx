3fdec90185cb31e5857fa12b09276c427fc8cc08
James Pike <jamesandrewpike@gmail.com>
2024-11-08 12:29:08 -0500

    commit

--- src/routes/(about)/about/index.tsx
+++ src/routes/(about)/about/index.tsx
@@ -24,11 +24,10 @@ export default component$(() => {
           <div class="absolute inset-0 flex items-center justify-center text-center text-white px-4 sm:px-6">
             <div class="max-w-3xl mx-auto lg:max-w-none">
               <h1 class="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter mb-4 font-heading dark:text-gray-200">
-                {/* Free template for <br class="hidden lg:block" />{" "} */}
-                <span class="hidden lg:inline">create a website using </span> <span class="text-[#039de1]">Qwik</span> +{" "}
+                Free template for <br class="hidden lg:block" />{" "}
+                <span class="hidden lg:inline">create a website using </span> <span class="text-[#039de1]">Qwik</span> &{" "}
                 <span class="sm:whitespace-nowrap text-[#039de1]">Tailwind</span>
-                <span class="hidden lg:inline">create a website using </span> <span class="text-[#039de1]">Qwik</span> +{" "}
-                <span class="sm:whitespace-nowrap text-[#039de1]">Tailwind</span>
+                
               </h1>
               {/* <p class="text-xl text-muted mb-6 dark:text-slate-300">
                 <span class="font-semibold underline decoration-wavy decoration-1 decoration-secondary-600 underline-offset-2">
@@ -37,7 +36,17 @@ export default component$(() => {
                 is a production ready template to start your new website using <em>Qwik</em> + <em>Tailwind CSS</em>.
                 It has been designed following Best Practices, SEO, Accessibility, Dark Mode, Great Page Speed, image optimization, sitemap generation and more.
               </p> */}
-
+<div class="flex justify-center gap-4 pt-5">
+                <a
+                  class="btn btn-primary"
+                  href="https://github.com/onwidget/qwind"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Get template
+                </a>
+                <button class="btn bg-gray-50 dark:bg-transparent">Learn more</button>
+              </div>
              
             </div>
           </div>
