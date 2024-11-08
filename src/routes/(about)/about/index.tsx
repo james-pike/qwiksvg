bf09889c349b63c01124a05944c90d5f49743234
James Pike <jamesandrewpike@gmail.com>
2024-11-07 20:52:23 -0500

    commit

--- src/routes/(about)/about/index.tsx
+++ src/routes/(about)/about/index.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
               <h1 class="text-5xl md:text-6xl font-bold leading-tighter tracking-tighter mb-4 font-heading dark:text-gray-200">
                 Free template for <br class="hidden lg:block" />{" "}
                 <span class="hidden lg:inline">create a website using </span> <span class="text-[#039de1]">Qwik</span> +{" "}
-                <span class="sm:whitespace-nowrap text-[#039de1]">Tailwind CSS</span>
+                <span class="sm:whitespace-nowrap text-[#039de1]">Tailwind</span>
               </h1>
               {/* <p class="text-xl text-muted mb-6 dark:text-slate-300">
                 <span class="font-semibold underline decoration-wavy decoration-1 decoration-secondary-600 underline-offset-2">
