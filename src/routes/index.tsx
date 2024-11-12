4f6ba966c85953e67363b3b3083ac53db629c76a
jvm3 <jamesandrewpike@gmail.com>
2024-11-12 09:41:53 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,7 +22,7 @@ export default component$(() => {
        
 
         <div class="relative w-full h-[55vh] sm:h-[90vh] overflow-hidden">
-        <script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script><dotlottie-player src="https://lottie.host/a746769e-b3c0-49f1-8db7-2e32d6cf2aee/B87BeustR2.json" background="transparent" speed="1" style="width: 100%; height: 100%" direction="1" playMode="normal" loop controls autoplay></dotlottie-player>
+        <script src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs" type="module"></script><dotlottie-player src="https://lottie.host/a746769e-b3c0-49f1-8db7-2e32d6cf2aee/B87BeustR2.json" background="transparent" speed="1" style="width: 100%; height: 100%" direction="1" playMode="normal" loop autoplay></dotlottie-player>
           <dotlottie-player
             src="https://lottie.host/31d07d4e-e255-4bfe-b875-3c165b58d6b0/YKqfIcJWuT.json"
             background="transparent"
